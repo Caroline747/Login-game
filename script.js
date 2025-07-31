60a75097cfb8a01cceab0411f9ad444dcc0184f7
Caroline <karolsamara97@gmail.com>
2025-07-31 18:05:41 -0300

    up

--- script.js
+++ script.js
@@ -1,2 +1,14 @@
 // script.js
 import "nes.css/css/nes.min.css";
+
+
+    const h = document.querySelector('head');
+    ['./lib/dialog-polyfill.css', './lib/highlight-theme.css'].forEach(a => {
+      const l = document.createElement('link');
+      l.href = a;
+      l.rel = '/stylesheet';
+      h.appendChild(l);
+    })
+    import './lib/dialog-polyfill.js';
+    import './lib/highlight.js';
+    import './lib/nes.css.js';  
